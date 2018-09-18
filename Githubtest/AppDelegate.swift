895e2054aef8bdd4feb2e5be57b771280f84422b
Erik Liebenberg <ierik@liebenberg.co.nz>
2018-09-18 14:02:49 +1200

    Initial Commit

---
+++ Githubtest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Githubtest
+//
+//  Created by Erik Liebenberg on 18/09/18.
+//  Copyright © 2018 Erik Liebenberg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Githubtest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Githubtest
+//
+//  Created by Erik Liebenberg on 18/09/18.
+//  Copyright © 2018 Erik Liebenberg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
